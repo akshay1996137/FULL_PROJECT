bf8132b2bddbd7488194152c3ae3a5b4958c0dcf
root <root@ip-172-31-21-63.ec2.internal>
2025-07-14 14:57:45 +0000

    FILES_ADDED

---
+++ dev.tfvars
@@ -0,0 +1,2 @@
+ instance_type = ["t2.micro", "t2.medium"]
+ instance_name = ["JENKINS-SERVER", "TOMCAT-SERVER"]
---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "PROJECT" {
+  count         = 2
+  ami           = "ami-0150ccaf51ab55a51"
+  instance_type = var.instance_type[count.index]
+  tags = {
+    Name = var.instance_name[count.index]
+  }
+}
+
+variable "instance_type" {
+
+}
+
+variable "instance_name" {
+  
+}
