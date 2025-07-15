54eaaaf42d5a408647518cc5d03a012eb2ecea62
akshay1996137 <83005391+akshay1996137@users.noreply.github.com>
2025-07-15 10:58:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,18 +3,20 @@ provider "aws" {
 }
 
 resource "aws_instance" "TERRA" {
-  count         = 2
+  count         = 3
   ami           = "ami-0150ccaf51ab55a51"
   instance_type = var.instance_type[count.index]
+  key_name = "avk"
+  vpc_security_group_ids = ["sg-084962ca46cb28cd8"]
   tags = {
     Name = var.instance_name[count.index]
   }
 }
 
 variable "instance_type" {
-  default = ["t2.micro", "t2.medium"]
+  default = ["t2.micro", "t2.micro", "t2.medium"]
 }
 
 variable "instance_name" {
-  default = ["TOMCAT", "JENKINS"]
+  default = ["TOMCAT", "MONITOR-SERVER" , "JENKINS"]
 }
