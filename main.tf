8ab27c32fde18462d27f10f6a11b2a9852a83c74
akshay1996137 <83005391+akshay1996137@users.noreply.github.com>
2025-07-16 11:11:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,13 +4,17 @@ provider "aws" {
 
 resource "aws_instance" "TERRA" {
   count         = 3
-  ami           = "ami-020cba7c55df1f615"
+  ami           = "var.ami"
   instance_type = var.instance_type[count.index]
   key_name = "avk"
   vpc_security_group_ids = ["sg-084962ca46cb28cd8"]
   tags = {
     Name = var.instance_name[count.index]
   }
+}
+
+variable "ami" {
+  default = ["ami-0150ccaf51ab55a51", "ami-0150ccaf51ab55a51", "ami-020cba7c55df1f615"]
 }
 
 variable "instance_type" {
